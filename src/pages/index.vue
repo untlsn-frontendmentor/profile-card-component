0069c28aebf700d5b991bba479f3d7d66fe86374
Filip Skoczeń <90275016+untlsn@users.noreply.github.com>
2022-10-07 20:51:23 +0200

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="min-h-screen grid place-items-center">
+    <div class="text-center">
+      <img src="/public/favicon.svg" alt="logo" class="w-60">
+      <div>Hello World!</div>
+    </div>
+  </main>
+</template>
